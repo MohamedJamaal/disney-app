09bb2df102f3100304abd23d88a1a680508c6977
MohamedJamaal <mohamed_gamal19@hotmail.com>
2022-03-18 01:08:47 +0200

    Add Login page and styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
-      <h2>Hello</h2>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Login />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
