b18317c15b4729fa0a9ff2a83a45555d8ce9c5b0
MohamedJamaal <mohamed_gamal19@hotmail.com>
2022-04-04 00:39:59 +0200

    Add Sign Out Functionality

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,35 @@
 import styled from 'styled-components';
+import ImgSlider from './ImgSlider';
 
 const Home = (props) => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Container>
+        <h2>
+          <ImgSlider />
+        </h2>
+      </Container>
+    </div>
+  );
 };
+
+const Container = styled.main`
+  position: relative;
+  min-height: calc(100vh-250px);
+  overflow: hidden;
+  display: block;
+  top: 72px;
+  padding: 0 calc(3.5vw+5px);
+
+  &:after {
+    background: url('/images/home-background.png') center center/cover no-repeat
+      fixed;
+    content: '';
+    position: absolute;
+    inset: 0px;
+    opacity: 1;
+    z-index: -1;
+  }
+`;
 
 export default Home;
