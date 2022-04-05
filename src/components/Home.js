5ef61a337b0415ab0b30eb77ef87539d2b3f4333
MohamedJamaal <mohamed_gamal19@hotmail.com>
2022-04-05 04:15:11 +0200

    Add slider

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,9 +5,7 @@ const Home = (props) => {
   return (
     <div>
       <Container>
-        <h2>
-          <ImgSlider />
-        </h2>
+        <ImgSlider />
       </Container>
     </div>
   );
@@ -15,15 +13,14 @@ const Home = (props) => {
 
 const Container = styled.main`
   position: relative;
-  min-height: calc(100vh-250px);
-  overflow: hidden;
+  min-height: calc(100vh - 250px);
+  overflow-x: hidden;
   display: block;
   top: 72px;
-  padding: 0 calc(3.5vw+5px);
-
+  padding: 0 calc(3.5vw + 5px);
   &:after {
-    background: url('/images/home-background.png') center center/cover no-repeat
-      fixed;
+    background: url('/images/home-background.png') center center / cover
+      no-repeat fixed;
     content: '';
     position: absolute;
     inset: 0px;
